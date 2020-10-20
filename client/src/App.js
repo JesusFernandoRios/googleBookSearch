ca0e8df2633d510521d8a49376a10e1fd8f43241
Jrios <Jesusfernandorios93@gmail.com>
2020-10-20 15:36:56 -0700

    decided to start over with src folder, decided to add pages instead of all components

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,28 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import "./App.css";
-import Header from "./component/Header";
-import SavedBooks from "./component/SavedBooks";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SaveBooks from "./pages/SaveBooks";
+import SearchBooks from "./pages/SearchBooks";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer"
+import "./App.css"
+
 
 function App() {
-  
-    return (
-      <Router>
+  return (
+    <Router>
+      <div>
+        <Nav />
         <Switch>
-          <Route path="/">
-          <Header/>
-          <SavedBooks/>
-          </Route>
+          <Route exact path="/" component={SearchBooks} />
+          <Route exact path="/saved" component={SaveBooks} />
+          <Route exact path="/saved/:id" component={SaveBooks} />
+          <Route component={NoMatch} /> 
         </Switch>
-      </Router>
-    );
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+	<footer className = "page-footer">
+		<div className = "container-fluid text-center">
+			<p>Duncan Hieu M. Dam</p>
+		</div>	
+	</footer>
+);
+
+export default Footer;
