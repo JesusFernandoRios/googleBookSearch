817533ac86ef29c3e2259bd82766c00084b67429
JesusFernandoRios <62573536+JesusFernandoRios@users.noreply.github.com>
2022-06-30 12:58:20 -0700

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,4 +5,4 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+//render has been replaced with createRoot in React 18.
